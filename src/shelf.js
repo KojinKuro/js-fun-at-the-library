3df97e72bd872333c83878599ae329cbba80edce
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-01-31 13:03:20 -0800

    Complete shelf.js with tests

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,25 @@
+function shelfBook(book, shelf) {
+  const MAX_SHELF_SIZE = 3;
+  if (shelf.length < MAX_SHELF_SIZE) shelf.unshift(book);
+}
+
+function unshelfBook(bookName, shelf) {
+  bookIndex = shelf.findIndex((b) => b.title === bookName);
+  shelf.splice(bookIndex,1);
+}
+
+function listTitles(shelf) {
+  const shelfNames = shelf.map((b) => b.title);
+  return shelfNames.join(', ');
+}
+
+function searchShelf(shelf, bookName) {
+  return shelf.find((b) => b.title === bookName) ? true : false;
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
