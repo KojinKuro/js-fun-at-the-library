57d211df4438b69229c2cc9a9071785b31bd661b
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-01-31 11:27:57 -0800

    Complete book.js with tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,44 @@
-function createTitle() {
-  
+function createTitle(bookName) {
+  if(bookName.split(' ')[0].toLowerCase() !== 'the') {
+    return `The ${bookName}`;
+  } return bookName;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, database) {
+  if(!database.find(e => e === review)) database.push(review);
+}
+
+function calculatePageCount(bookName) {
+  const PAGE_MULTIPLIER = 20;
+  return bookName.length * PAGE_MULTIPLIER;
+}
+
+function writeBook(title, mainCharacter, genre) {
+  return {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+}
+
+function editBook(book) {
+  book.pageCount = Math.floor(book.pageCount * 0.75);
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
